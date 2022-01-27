312b93106d53bc94d853e669e299a9bdefdf9a02
vanhuycx <vanhuycx@gmail.com>
2022-01-27 10:26:53 -0500

    Remove unnecesscary icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
 import { useState } from 'react'
 import './App.css'
-import btc_logo from './btc_icon.svg'
+import btc_icon from './btc_icon.svg'
 
 function App() {
 
   return (
       <>
-      <div className="App">
-        <div className="App-header">
+        <div className="App">
 
-          <div className="Logo-section">
-            <div><img className="App-logo" src={btc_logo} alt="logo"/></div>
-            <div>Crypto World</div>
+          <div className="App-header">
+            <div className="Logo-section">
+              <div><img className="App-logo" src={btc_icon} alt="logo"/></div>
+              <div>Crypto World</div>
+            </div>
+            <div>Home</div> 
+            <div>Exchange </div> 
+            <div>News</div> 
+            <div>Fear & Greed Index </div> 
           </div>
 
-          <div>Home</div> 
+          <div>
+            <h1>Content</h1>
+          </div>
           
-          <div>Exchange </div> 
-          <div>News</div> 
-          <div>Fear & Greed Index </div> 
-
-        </div>
-        
-        <div>
-          <h1>Content</h1>
-        </div>
-        
-
-      </div>
-      
-   
-
-</>
+        </div>    
+      </>
   )
 }
 
