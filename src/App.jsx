7ae3aeb9bf7994bbe541a679930d000c68add85a
vanhuycx <vanhuycx@gmail.com>
2022-01-28 20:47:46 -0500

    Add content to the homepage. Add services folder with crypto API inside

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ const App = () => {
                 <Link to='/news'>News</Link>
               </Space>
             </div>
+            
           </div>
         </div>    
       </>
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,10 @@
+var axios = require("axios").default;
+
+var options = {
+  method: 'GET',
+  url: 'https://coingecko.p.rapidapi.com/exchanges',
+  headers: {
+    'x-rapidapi-host': 'coingecko.p.rapidapi.com',
+    'x-rapidapi-key': 'd15052e859msh9a97166e104b84ap1a1193jsn40d1de61b5d6'
+  }
+};
