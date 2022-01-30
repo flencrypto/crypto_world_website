be4f5c2dadd3f35b104681bb63904f1ad71d1388
vanhuycx <vanhuycx@gmail.com>
2022-01-30 12:11:26 -0500

    Implement search bar

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card,Row,Col, Input } from "antd";
@@ -9,13 +9,30 @@ const Cryptocurrencies = ({simplified}) => {
   const {data:cryptoList,isFetching} = useGetCryptosQuery(count);
 //  console.log(cryptoList)
 
-  const [cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
+  useEffect(() => {
+    setCryptos(cryptoList?.data?.coins);
+
+    const filteredData = cryptoList?.data?.coins.filter((coin)=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+    setCryptos(filteredData);
+
+  }, [cryptoList,searchTerm]);
+
+    
+  
   // console.log(cryptos)
   if (isFetching) return '...Loading';
 
+
   return (
       <>
+        <div className="search-crypto">
+          <Input placeholder="Search Cryptocurrency" onChange={(e)=>setSearchTerm(e.target.value)}/>
+        </div>
+
         <Row gutter={[32,32]} className="crypto-card-container"> 
           {cryptos?.map((currency)=>(
             <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
