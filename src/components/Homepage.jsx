b341976b6273bce2ca878d47c727ea25dee3c0cb
vanhuycx <vanhuycx@gmail.com>
2022-02-06 10:08:38 -0500

    Add ICO for stat and fix font-weight

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -19,6 +19,8 @@ const Homepage = () => {
 
   const {data:globalStats,isFetching} = useGetGlobalStatCoingeckoQuery();
 
+  const globalData = globalStats?.data
+
   console.log(globalStats?.data)
 
   if (isFetching) return 'Loading...'
@@ -29,17 +31,24 @@ const Homepage = () => {
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
-      <Row>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={(globalStats?.data)?globalStats?.data?.active_cryptocurrencies:'No data'}/></Col>
+      <Row className="stat">
+        <Col span={12}>
+          <Statistic  title= {'Total Cryptocurrencies'} value={(globalData)?globalData?.active_cryptocurrencies:'No data'}/>
+        </Col>
+
         <Col span={12}> 
-           <Statistic title={`Market Dominance`} value={(globalStats?.data)?'BTC: ' + globalStats?.data?.market_cap_percentage?.btc.toLocaleString("en-US",{maximumFractionDigits: 2})+'% - ETH: ' + globalStats?.data?.market_cap_percentage?.eth.toLocaleString("en-US",{maximumFractionDigits: 2}) + '%' :'No data'}/> 
-          </Col>
+           <Statistic title={`Market Dominance`} value={(globalData)?'BTC: ' + globalData?.market_cap_percentage?.btc?.toLocaleString("en-US",{maximumFractionDigits: 2})+'% - ETH: ' + globalData?.market_cap_percentage?.eth?.toLocaleString("en-US",{maximumFractionDigits: 2}) + '%' :'No data'}/> 
+        </Col>
         <Col span={12}><Statistic title={`Total Market Cap`}
-                          value={globalStats?(globalStats?.data?.total_market_cap?.usd).toLocaleString("en-US",{style: "currency",currency: "usd"}):'No data'}/>
+                          value={globalData?(globalData?.total_market_cap?.usd).toLocaleString("en-US",{style: "currency",currency: "usd"}):'No data'}/>
         </Col>
         <Col span={12}><Statistic title="Total 24h Volume" 
-                          value={globalStats?(globalStats?.data?.total_volume?.usd).toLocaleString("en-US",{style: "currency",currency: "usd"}):'No data'}/></Col>
-        <Col span={12}><Statistic title="Total Markets" value={globalStats?globalStats?.data?.markets:'No data'}/></Col>
+                          value={globalData?(globalData?.total_volume?.usd).toLocaleString("en-US",{style: "currency",currency: "usd"}):'No data'}/></Col>
+        <Col span={12}><Statistic title="Total Markets" value={globalStats?globalData?.markets:'No data'}/></Col>
+
+        <Col span={12}> 
+           <Statistic title={`Initial Coin Offering (ICO)`} value={(globalData)?'Ongoing: ' + globalData?.ongoing_icos + ' - Ended: ' + globalData?.ended_icos :'No data'}/> 
+          </Col>
       </Row>
 
       <div className="home-heading-container">
