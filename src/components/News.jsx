a94c19a2e4212c2bbceba542a2a56713bb94b39f
vanhuycx <vanhuycx@gmail.com>
2022-02-07 23:07:00 -0500

    Use different API for News

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,9 +10,10 @@ const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=Ne
 const News = ({simplified}) => {
   const onPage = 'News';
   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  const count = simplified?6:12;
+  const page_size = simplified ? 6 : 25;
 
-  const {data:cryptoNews,isFetching} = useGetNewsQuery({newsCategory:newsCategory,count:count})
+  const {data:cryptoNews,isFetching} = useGetNewsQuery({newsCategory:newsCategory,page_size:page_size})
+  console.log(cryptoNews)
 
   if (isFetching) return 'Loading'
 
@@ -30,26 +31,28 @@ const News = ({simplified}) => {
       )}
 
       <Row gutter={[24,24]}>
-          {cryptoNews?.value?.length > 0 ? 
+          {cryptoNews?.articles?.length > 0 ? 
             (   
-              cryptoNews?.value?.map((news,i)=>(
+              cryptoNews?.articles?.map((news,i)=>(
                     <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className="news-card">
-                          <a href={news?.url} target="blank" rel="noreferrer">
+                          <a href={news?.link} target="blank" rel="noreferrer">
                             <div className="news-image-container">
-                              <Title className="news-title" level={4}>{news?.name}</Title>
-                              <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+                              <Title className="news-title" level={4}>{news?.title}</Title>
+                              <img className="news-image" src={news?.media || demoImage} alt="" />
                             </div> 
                             <p>
-                              {news?.description >100? `${news?.description?.substring(0,100)}...`: news.description || ''}
+                              {news?.summary?.length >100? `${news?.summary?.substring(0,100)}...`: news?.summary || ''}
                             </p>
                             <div className="provider-container">
                               <div>
-                                <Avatar src={news?.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt=""/>
-                                <Text>{news?.provider[0]?.name}</Text>
+                                {/* <Avatar src={news?.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt=""/> */}
+                                <Text>{news?.authors[0] || ''}</Text>
                               </div>
+
+                                <Text>{news?.published_date||''}</Text>
                     
-                                <Text>{moment(news?.datePublished).startOf('ss').fromNow()}</Text>
+                                {/* <Text>{moment(news?.datePublished).startOf('ss').fromNow()}</Text> */}
                             </div>
                             
                           </a> 
--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -2,13 +2,12 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 // For Bing news
 const newsApiHeader = { 
-    'x-bingapis-sdk': 'true',
-    'x-rapidapi-host': process.env.REACT_APP_BING_RAPIDAPI_HOST,
-    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_API_KEY_test
+    'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_API_KEY
 
 }
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+const baseUrl = 'https://free-news.p.rapidapi.com'
 
 const createRequest = (url)=> ({url,headers:newsApiHeader});
 
@@ -17,7 +16,7 @@ export const newsApi = createApi({
     baseQuery: fetchBaseQuery({baseUrl:baseUrl}),
     endpoints: (builder)=>({
         getNews:builder.query({
-            query: ({newsCategory,count}) => createRequest(`/news/search?q=${newsCategory}&freshness=Day&textFormat=Raw&safeSearch=Off&count=${count}`),
+            query: ({newsCategory,page_size}) => createRequest(`/v1/search?q=${newsCategory}&page_size=${page_size}&lang=en`),
         })
     })
 })
