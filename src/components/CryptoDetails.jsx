fc632237b5b3cbb169b02589380b38cf62eefb22
vanhuycx <vanhuycx@gmail.com>
2022-02-08 22:45:22 -0500

    Change the icons

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -3,7 +3,7 @@ import HTMLReactParser from "html-react-parser";
 import {useParams} from 'react-router-dom';
 import millify from 'millify';
 import {Col,Row,Typography,Select} from 'antd'
-import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined,QuestionOutlined } from '@ant-design/icons';
+import { DollarCircleOutlined, FundOutlined,SlidersOutlined, ExclamationCircleOutlined, RiseOutlined,FallOutlined, TrophyOutlined, NumberOutlined, ThunderboltOutlined,QuestionOutlined, VerticalAlignBottomOutlined } from '@ant-design/icons';
 import { useGetSpecificCoinQuery } from "../apiServices/cryptoApi";
 
 const {Title,Text} = Typography;
@@ -22,22 +22,50 @@ const CryptoDetails = () => {
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
   const stats = [
-    { title: 'Price to USD', value: cryptoDetails?.current_price?.usd ? cryptoDetails?.current_price?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}):<QuestionOutlined/>, icon: <DollarCircleOutlined /> },
-    { title: 'Market Cap Rank', value: cryptoDetails?.market_cap_rank? cryptoDetails?.market_cap_rank : <QuestionOutlined/> , icon: <NumberOutlined /> },
-    { title: '24h Volume', value: cryptoDetails?.total_volume?.usd ? cryptoDetails?.total_volume?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0})  : <QuestionOutlined/>, icon: <ThunderboltOutlined /> },
-    { title: 'Market Cap', value: cryptoDetails?.market_cap?.usd ? cryptoDetails?.market_cap?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, icon: <DollarCircleOutlined /> },
-    { title: '24h High', value: cryptoDetails?.high_24h?.usd ? cryptoDetails?.high_24h?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, icon: <MoneyCollectOutlined /> },
-    { title: 'Price Change Percentage 24h', value: cryptoDetails?.price_change_percentage_24h ? cryptoDetails?.price_change_percentage_24h?.toLocaleString("en-US",{style: "decimal",maximumFractionDigits: 2}) + '%' : <QuestionOutlined/>, icon: <TrophyOutlined /> },
-    { title: 'All-time High', value: cryptoDetails?.ath?.usd ? cryptoDetails?.ath?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, icon: <FundOutlined /> },
-    { title: 'All-time Low', value: cryptoDetails?.atl?.usd ? cryptoDetails?.atl?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, icon: <TrophyOutlined /> },
+    { title: 'Market Cap Rank', 
+    value: cryptoDetails?.market_cap_rank? cryptoDetails?.market_cap_rank : <QuestionOutlined/>, 
+    icon: <NumberOutlined /> },
+
+    { title: 'Price to USD', 
+    value: cryptoDetails?.current_price?.usd ? cryptoDetails?.current_price?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 2}):<QuestionOutlined/>, 
+    icon: <DollarCircleOutlined /> },
+
+    { title: 'Price Change Percentage 24h', 
+    value: cryptoDetails?.price_change_percentage_24h ? cryptoDetails?.price_change_percentage_24h?.toLocaleString("en-US",{style: "decimal",maximumFractionDigits: 2}) + '%' : <QuestionOutlined/>, 
+    icon: <SlidersOutlined /> },
+
+
+    { title: '24h Volume', 
+    value: cryptoDetails?.total_volume?.usd ? cryptoDetails?.total_volume?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0})  : <QuestionOutlined/>, 
+    icon: <ThunderboltOutlined /> },
+
+    { title: 'Market Cap', 
+    value: cryptoDetails?.market_cap?.usd ? cryptoDetails?.market_cap?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, 
+    icon: <FundOutlined /> },
+
+    { title: '24h High', 
+    value: cryptoDetails?.high_24h?.usd ? cryptoDetails?.high_24h?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, 
+    icon: <RiseOutlined /> },
+
+    { title: '24h Low', 
+    value: cryptoDetails?.low_24h?.usd ? cryptoDetails?.low_24h?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, 
+    icon: <FallOutlined /> },
+
+    { title: 'All-time High', 
+    value: cryptoDetails?.ath?.usd ? cryptoDetails?.ath?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, 
+    icon: <TrophyOutlined /> },
+
+    { title: 'All-time Low', 
+    value: cryptoDetails?.atl?.usd ? cryptoDetails?.atl?.usd?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined/>, 
+    icon: <VerticalAlignBottomOutlined /> },
   ];
 
   const genericStats = [
     // { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
     // { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    { title: 'Max Supply', value: `${cryptoDetails?.max_supply ? <CheckOutlined /> : <StopOutlined />}`, icon: <ExclamationCircleOutlined /> },
-    { title: 'Total Supply', value: `${cryptoDetails?.total_supply ? cryptoDetails?.total_supply: <QuestionOutlined/>}`, icon: <ExclamationCircleOutlined /> },
-    { title: 'Circulating Supply', value: `${cryptoDetails?.circulating_supply ? cryptoDetails?.circulating_supply:<QuestionOutlined/>}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Max Supply', value: cryptoDetails?.max_supply ? cryptoDetails?.max_supply?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}) : <QuestionOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Total Supply', value: cryptoDetails?.total_supply ? cryptoDetails?.total_supply?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}): <QuestionOutlined/>, icon: <ExclamationCircleOutlined /> },
+    { title: 'Circulating Supply', value: cryptoDetails?.circulating_supply ? cryptoDetails?.circulating_supply?.toLocaleString("en-US",{style: "currency",currency: "usd",maximumFractionDigits: 0}):<QuestionOutlined/>, icon: <ExclamationCircleOutlined /> },
   ];
 
   return (
@@ -65,6 +93,26 @@ const CryptoDetails = () => {
           </Col>
 
           {stats?.map(({title,value,icon})=>(
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className="stats">{value}</Text>
+            </Col>
+          ))}
+
+
+        </Col>
+
+
+        <Col className="other-stats-info">
+          <Col className="coin-value-statistics-heading">
+            <Title level={3} className="coin-details-heading">{data?.name} Supply Statistics</Title>
+            <p>Supply statistics of {data?.name} such as Max, Total, and Circulating Supply</p>
+          </Col>
+
+          {genericStats?.map(({title,value,icon})=>(
             <Col className="coin-stats">
               <Col className="coin-stats-name">
                 <Text>{icon}</Text>
