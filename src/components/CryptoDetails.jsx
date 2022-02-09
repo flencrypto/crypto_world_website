daf1ec1d9d36c51f3f399a44df099a6e383fd6b0
vanhuycx <vanhuycx@gmail.com>
2022-02-08 21:07:30 -0500

    Get the right key for cryptoDetail data

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -14,6 +14,7 @@ const CryptoDetails = () => {
   const [timePeriod, setTimePeriod] = useState('7d');
   const {data,isFetching} = useGetSpecificCoinQuery({coinId:coinId},{pollingInterval:30000});
   const cryptoDetails = data?.market_data;
+  console.log(cryptoDetails)
 
 
   if (isFetching) return 'Loading...'
@@ -21,19 +22,22 @@ const CryptoDetails = () => {
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
   const stats = [
-    { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
-    { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-    { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
-    { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
-    { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
+    { title: 'Price to USD', value: `${cryptoDetails?.current_price?.usd ? '$' + cryptoDetails?.current_price?.usd:'No data'}`, icon: <DollarCircleOutlined /> },
+    { title: 'Market Cap Rank', value: cryptoDetails?.market_cap_rank? cryptoDetails?.market_cap_rank : 'No data' , icon: <NumberOutlined /> },
+    { title: '24h Volume', value: `${cryptoDetails?.total_volume ? '$' + cryptoDetails?.total_volume  : 'No data'}`, icon: <ThunderboltOutlined /> },
+    { title: 'Market Cap', value: `${cryptoDetails?.market_cap ? '$' +  cryptoDetails?.market_cap : 'No data'}`, icon: <DollarCircleOutlined /> },
+    { title: '24h High', value: `${cryptoDetails?.high_24h?.usd ? '$' +  cryptoDetails?.high_24h?.usd : 'No data'}`, icon: <MoneyCollectOutlined /> },
+    { title: 'Price Change Percentage 24h', value: ` ${cryptoDetails?.price_change_percentage_24h ? cryptoDetails?.price_change_percentage_24h + '%' : 'No data'}`, icon: <TrophyOutlined /> },
+    { title: 'All-time High', value: `${cryptoDetails?.ath?.usd ? '$' + cryptoDetails?.ath?.usd : 'No data'}`, icon: <FundOutlined /> },
+    { title: 'All-time Low', value: `$ ${cryptoDetails?.atl?.usd ? '$' + cryptoDetails?.atl?.usd : 'No data'}`, icon: <TrophyOutlined /> },
   ];
 
   const genericStats = [
-    { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
-    { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    { title: 'Aprroved Supply', value: cryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-    { title: 'Total Supply', value: `$ ${cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
-    { title: 'Circulating Supply', value: `$ ${cryptoDetails?.supply?.circulating && millify(cryptoDetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
+    // { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
+    // { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+    { title: 'Max Supply', value: `${cryptoDetails?.max_supply ? <CheckOutlined /> : <StopOutlined />}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Total Supply', value: `${cryptoDetails?.total_supply ? cryptoDetails?.total_supply: 'No data'}`, icon: <ExclamationCircleOutlined /> },
+    { title: 'Circulating Supply', value: `${cryptoDetails?.circulating_supply ? cryptoDetails?.circulating_supply:'No data'}`, icon: <ExclamationCircleOutlined /> },
   ];
 
   return (
